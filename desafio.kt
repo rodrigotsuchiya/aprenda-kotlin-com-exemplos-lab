eb83ece455cab1f1043439cc79ede423e8775663
rodrigo tsuchiya <37986367+rodrigotsuchiya@users.noreply.github.com>
2024-01-11 15:22:17 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,26 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int)
+
+data class Usuario(var nome:String)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        inscritos += listOf(usuario)
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+fun main() {    
+    var matematica = ConteudoEducacional("matematica", 40)
+    val materia = listOf(matematica)
+    var formacao = Formacao("Exatas", materia)
+    val aluno1 = Usuario("Aluno1")
+    val alunoMatricula = formacao.matricular(aluno1)
+    
+ }
